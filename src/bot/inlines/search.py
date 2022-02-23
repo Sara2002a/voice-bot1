1b305f1f378eb43ceff4c1900da7c19220092dd8
jetwolf <jetwolf@jet-technology.fun>
2022-02-23 16:53:27 +0500

    feat: add search by title and performer in inline mode

--- src/bot/inlines/search.py
+++ src/bot/inlines/search.py
@@ -1,14 +1,30 @@
-from telegram import InlineQueryResultAudio, Update
+from asyncio.log import logger
+from operator import or_
+from telegram import InlineQueryResultAudio, Update, constants
 from telegram.ext import CallbackContext
-
+from sqlalchemy import or_
 from bot.utils import check_user
-from models import voice_model
+from models import voice_model, voices
 from settings import database
 
 
 @check_user
 def search(update: Update, context: CallbackContext) -> None:
     offset = 0 if not update.inline_query.offset else int(update.inline_query.offset)
+    voices = (
+        voice_model.select()
+        .limit(constants.MAX_INLINE_QUERY_RESULTS)
+        .offset(offset * constants.MAX_INLINE_QUERY_RESULTS)
+    )
+
+    if text_search := update.inline_query.query:
+        voices = voices.where(
+            or_(
+                voice_model.c.title.like(f"%{text_search}%"),
+                voice_model.c.performer.like(f"%{text_search}%"),
+            )
+        )
+
     update.inline_query.answer(
         [
             InlineQueryResultAudio(
@@ -17,7 +33,7 @@ def search(update: Update, context: CallbackContext) -> None:
                 audio_url=row["link"],
                 performer=row["performer"],
             )
-            for row in database.execute(voice_model.select().limit(50).offset(offset * 50))
+            for row in database.execute(voices)
         ],
         timeout=10,
         next_offset=offset + 1,
